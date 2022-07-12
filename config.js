8dee6beabb17d796ac2897a038fc4a166e1ba681
Pxmatija <pazdan.osram@gmail.com>
2022-07-12 15:24:23 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+const GAME_WIDTH = 1280
+const GAME_HEIGHT = 820
+
+const _Modded = false
+
+//I dont recomend to use this variable
+const _DEBUG_ = false
+
+export {
+	kaboom, GAME_HEIGHT, GAME_WIDTH, _Modded, _DEBUG_
+}
